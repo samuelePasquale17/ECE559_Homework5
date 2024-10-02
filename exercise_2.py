893b6cf4eb1676c52f67b4d2ead11594f85a7ac5
Samuele Pasquale <samuele.pasquale01@gmail.com>
2024-10-01 20:50:25 -0500

    Final commit

--- exercise_2.py
+++ exercise_2.py
@@ -37,9 +37,7 @@ def gradient_z_l(del_f, U):
 
     gradient_z_l =  U^T * del_f
     :param del_f: dimension 1x1
-    :param U: dimensione 1x3
-    :param Z: matrix 3x1
-
+    :param U: dimension 1x3
     :return: gradient of the loss function respect to z (dimension 3x1)
     """
     # return gradient_z_l
@@ -67,6 +65,7 @@ def delta_z(grad_z_l, vz, a):
         res.append(sig_der[i] * grad_z_l[i][0])
 
     return np.array(res).reshape(-1, 1)
+
 
 def v_z(W, X, b):
     """
@@ -183,7 +182,7 @@ def generate_data_and_plot():
     return x1, x2, y
 
 
-def plot_mse_epoch(MSE_results):
+def plot_mse_epoch(MSE_results, eta):
     """
     Function to plot the MSE vs. epochs
     """
@@ -192,77 +191,43 @@ def plot_mse_epoch(MSE_results):
         y_mse.append(mse[0][0])
 
     plt.plot(range(1, len(y_mse) + 1), y_mse, marker='o', linestyle='-', color='b')
-    plt.title('MSE vs. Epochs')
+    plt.title(f'MSE vs. Epochs with eta = 0.01 and dev. standard = {eta}')
     plt.xlabel('Epochs')
     plt.ylabel('Mean Squared Error (MSE)')
     plt.grid(True)
     plt.show()
 
 
-def training_NN_forward_backward(x1, x2, y, num_epochs, W_rows, W_cols, avg, var, b_rows, b_cols, U_rows, U_cols, c_rows, c_cols, a, eta):
+def training_NN_forward_backward(x1, x2, y, num_epochs, W_rows, W_cols, avg, var, b_rows, b_cols, U_rows, U_cols, c_rows, c_cols, a, eta, eta_dim_factor, eta_mod, initRand, initVal, minibatch_en, minibatch_size):
     """
     Function that given a data set, trains the NN with forward and backward propagation
-    :return:
-    """
-    # random initialization of weights and biases
-    W = initialize_matrix(W_rows, W_cols, avg, var)
-    b = initialize_matrix(b_rows, b_cols, avg, var)
-    U = initialize_matrix(U_rows, U_cols, avg, var)
-    c = initialize_matrix(c_rows, c_cols, avg, var)
-
-    # MSE list over epochs
-    MSE_results = []
-
-    # run over data (epochs)
-    for epoch in range(num_epochs):
-        # init mse per epoch
-        mse_epoch = 0
-        # pass over data set
-        for x1_val, x2_val, y_val in zip(x1, x2, np.array(y)):
-
-
-            # Forward (x -> vz -> z -> vf -> f)
-            x = [[x1_val], [x2_val]]
-            vz = v_z(W, x, b)
-            z_vect = z(vz, a)
-            vf = v_f(U, z_vect, c)
-            f_out = f(vf, a)
-
-            # Backward (grad_b_l <- grad_W_l <- delta_z <- grad_z_l <- grad_c_l <- grad_U_l <- delta_f <- grad_f_l)
-            grad_f_l  = gradient_f_l(f_out, y_val)
-            del_f = delta_f(grad_f_l, vf, a)
-            grad_u_l = np.array(del_f) @ np.array(z_vect).T
-            grad_c_l = del_f
-            grad_z_l = gradient_z_l(del_f, U)
-            del_z = delta_z(grad_z_l, vz, a)
-            grad_w_l = np.array(del_z) @ np.array(x).T
-            grad_b_l = del_z
-
-            # gradient descend
-            W = W - eta * grad_w_l
-            b = b - eta * grad_b_l
-            U = U - eta * grad_u_l
-            c = c - eta * grad_c_l
-
-            # Accumulate squared error for MSE
-            mse_epoch += (f_out - y_val) ** 2
-
-        # Compute MSE for the current epoch and add to the list
-        mse_epoch /= len(y)  # Divide by the number of samples to get the average
-        MSE_results.append(mse_epoch)
-
-    # Plot MSE here
-    plot_mse_epoch(MSE_results)
-    return W, b, U, c
-
-
-def training_NN_forward_backward_v2(x1, x2, y, num_epochs, W_rows, W_cols, avg, var, b_rows, b_cols, U_rows, U_cols, c_rows, c_cols, a, eta, eta_dim_factor, eta_mod, initRand, initVal, minibatch_en, minibatch_size):
-    """
-    Function that given a data set, trains the NN with forward and backward propagation
-    :return:
+    :param x1: dataset x1
+    :param x2: dataset x2
+    :param y: dataset y
+    :param num_epochs: number of epochs
+    :param W_rows: number of rows in W
+    :param W_cols: number of columns in W
+    :param avg: average for the random generation of weights and biases
+    :param var: deviation standard for the random generation of weights and biases
+    :param b_rows: number of rows in b
+    :param b_cols: number of columns in b
+    :param U_rows: number of rows in U
+    :param U_cols: number of columns in U
+    :param c_rows: number of rows in c
+    :param c_cols: number of columns in c
+    :param a: parameter of the sigmoid function
+    :param eta: learning rate
+    :param eta_dim_factor: factor for updating learning algorithm runtime based on the MSE
+    :param eta_mod: flag, if True learning algorithm is updated runtime
+    :param initRand: flag, if True the initialization of weights and biases is randomly
+    :param initVal: if flag initRand is False, all weights and biases are initialized with this value
+    :param minibatch_en: flag, if active minibatch or Proper gradient descend is activated
+    :param minibatch_size: size of the minibatch, if Proper gradient descend is needed, set the size equal to the number of epochs
+    :return: Weights and biases
     """
     if initRand:
         # random initialization of weights and biases
+        np.random.seed(1713)
         W = initialize_matrix(W_rows, W_cols, avg, var)
         b = initialize_matrix(b_rows, b_cols, avg, var)
         U = initialize_matrix(U_rows, U_cols, avg, var)
@@ -273,7 +238,6 @@ def training_NN_forward_backward_v2(x1, x2, y, num_epochs, W_rows, W_cols, avg, 
         b = np.full((b_rows, b_cols), initVal)
         U = np.full((U_rows, U_cols), initVal)
         c = np.full((c_rows, c_cols), initVal)
-
 
     # MSE list over epochs
     MSE_results = []
@@ -289,8 +253,6 @@ def training_NN_forward_backward_v2(x1, x2, y, num_epochs, W_rows, W_cols, avg, 
         grad_b_l_acc = 0
         grad_u_l_acc = 0
         grad_c_l_acc = 0
-
-
 
         # pass over data set
         for i, (x1_val, x2_val, y_val) in enumerate(zip(x1, x2, np.array(y))):
@@ -323,18 +285,26 @@ def training_NN_forward_backward_v2(x1, x2, y, num_epochs, W_rows, W_cols, avg, 
             # check minibatch size
             if minibatch_en and (i + 1) % minibatch_size == 0:
                 # end of the minibatch, update gradients and zero-out accumulators
-                W = W - eta * grad_w_l_acc
-                b = b - eta * grad_b_l_acc
-                U = U - eta * grad_u_l_acc
-                c = c - eta * grad_c_l_acc
+                W = W - eta * (grad_w_l_acc/minibatch_size)
+                b = b - eta * (grad_b_l_acc/minibatch_size)
+                U = U - eta * (grad_u_l_acc/minibatch_size)
+                c = c - eta * (grad_c_l_acc/minibatch_size)
+                # zeroed-out accumulators for gradient
+                grad_w_l_acc = 0
+                grad_b_l_acc = 0
+                grad_u_l_acc = 0
+                grad_c_l_acc = 0
+
             elif not minibatch_en:
                 # gradient descend
                 W = W - eta * grad_w_l
                 b = b - eta * grad_b_l
                 U = U - eta * grad_u_l
                 c = c - eta * grad_c_l
+
             # Accumulate squared error for MSE
             mse_epoch += (f_out - y_val) ** 2
+
         # Compute MSE for the current epoch and add to the list
         mse_epoch /= len(y)  # Divide by the number of samples to get the average
         MSE_results.append(mse_epoch)
@@ -346,12 +316,13 @@ def training_NN_forward_backward_v2(x1, x2, y, num_epochs, W_rows, W_cols, avg, 
         # Update previous MSE for the next epoch
         previous_mse = mse_epoch
     # Plot MSE here
-    plot_mse_epoch(MSE_results)
+    plot_mse_epoch(MSE_results, var)
     return W, b, U, c
 
 
-def plot_3D_decision_boundary(x1, x2, y_pred):
+def plot_3D_decision_boundary(x1, x2, y_pred, title):
     fig = plt.figure(figsize=(7, 10))
+    plt.title(title)
     ax = plt.axes(projection="3d")
     ax.scatter3D(x1, x2, y_pred, color="green")
     ax.set_xlabel('x1')
@@ -379,7 +350,32 @@ def main():
     x1, x2, y = generate_data_and_plot()
 
     # training with forward and backward propagation
-    W, b, U, c = training_NN_forward_backward(x1, x2, y, 100, 3, 2, 0, 0.1, 3, 1, 1, 3, 1, 1, 5, 0.01)
+    W, b, U, c = training_NN_forward_backward(
+        x1,  # x1
+        x2,  # x2
+        y,  # y
+        100,  # number of epochs
+        3,  # W rows
+        2,  # W cols
+        0,  # average random generation
+        0.1,  # standard deviation random generation
+        3,  # b rows
+        1,  # b cols
+        1,  # U rows
+        3,  # U cols
+        1,  # c rows
+        1,  # c cols
+        5,  # sigmoid function parameter
+        0.01,  # eta gradient descend
+        0.95,  # change factor for eta if MSE is positive and eta_mod flag activated
+        False,  # enable eta value modulable based on MSE
+        True,  # random initialization of weights and biases
+        0,  # if random initialization deactivated, all weights and biases are initialized to initVal
+        False,  # enable minibatch, must be true is we want either minibatches or gradient descend with
+                            # cumulative gradients, updated after each epoch
+        0  # minibatch size, if we want gradient descend with cumulative gradients,
+                        # updated after each epoch, set it to the size of the data set
+    )
 
     # compute y_pred over the data set
     y_pred_NN = []
@@ -387,10 +383,20 @@ def main():
         y_pred_NN.append(neural_network(np.array([[x1_val], [x2_val]]), W, b, U, c, 5))
 
     # Visualize the decision boundary in 3D
-    plot_3D_decision_boundary(x1, x2, y_pred_NN)
+    plot_3D_decision_boundary(x1, x2, y_pred_NN, "3D decision boundary")
 
-
-
+    ###################################################
+    ##################### Point f #####################
+    ###################################################
+    # Possible improvements shown in the report for simplicity
+    # Different η values
+    # Variable η based on the MSE value
+    # Different a parameters of the sigmoid function
+    # Different number of epochs
+    # Proper gradient descent
+    # Minibatches
+    # Different architecture
+    # Change in the standard deviation for the random generation of weights and biases
 
 
 main()
